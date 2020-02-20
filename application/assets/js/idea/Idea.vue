8265665a789d1d9f389a230a1ee5289ced034693
Florian Abélard <f.abelard@gmail.com>
2020-02-20 18:10:05 +0100

    feat: create idea component

--- application/assets/js/idea/Idea.vue
+++ application/assets/js/idea/Idea.vue
@@ -1,12 +1,50 @@
 <template>
     <div>
-        <p>This is an example of a new components in VueJs</p>
+        <form @submit.prevent="addIdea">
+            Nouvelle idée cadeau : <br/><br/>
+            <input v-model="label" name="label" placeHolder="Libellé"/>
+            <input :disabled="!label" type="submit">
+        </form>
+        <br/>
+        <p>Voici la liste de vos idées cadeaux : </p>
+        <ul v-if="ideas.length">
+            <li v-for="idea in ideas">
+                {{ idea.label }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
-        name: "idea"
+        name: "idea",
+        data() {
+            return { ideas: [], label: '' };
+        },
+        created() {
+            this.fetchIdeas();
+        },
+        methods: {
+            fetchIdeas() {
+                fetch('/api/ideas')
+                .then( response => response.json() )
+                .then( (data) => {
+                    this.ideas = data['hydra:member'];
+                });
+            },
+            addIdea() {
+                const label = this.label;
+                fetch('/api/ideas', {
+                    method: 'POST',
+                    headers: {'Content-type': 'application/ld+json'},
+                    body: JSON.stringify({label})
+                })
+                .then( (result) => {
+                    this.fetchIdeas();
+                    this.label = '';
+                })
+            }
+        }
     }
 </script>
 
