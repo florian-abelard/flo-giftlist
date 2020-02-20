e9241749caaa2f922e99ec245475cb550cb158f8
Florian Abélard <f.abelard@gmail.com>
2020-02-20 18:11:33 +0100

    feat: create association between idea and recipient

--- application/src/Entity/Idea.php
+++ application/src/Entity/Idea.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -25,6 +26,20 @@ class Idea
      * @ORM\Column(type="string", length=255)
      */
     private $label;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Recipient")
+     * @ORM\JoinTable(name="ideas_recipients",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="recipient_id", referencedColumnName="id")}
+     *      )
+     */
+    private $recipients;
+
+    public function __construct()
+    {
+        $this->recipients = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
