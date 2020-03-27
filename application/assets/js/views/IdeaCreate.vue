b58b56f0ebe3dc50714300b684a07b0cbfe221c3
Florian Abélard <f.abelard@gmail.com>
2020-03-27 19:11:56 +0100

    feat(idea): create simple idea

--- application/assets/js/views/IdeaCreate.vue
+++ application/assets/js/views/IdeaCreate.vue
@@ -6,7 +6,24 @@
 
         Créer une idée cadeaux
 
+        <v-form>
 
+            <v-text-field
+                v-model="label"
+                label="Libellé"
+                required
+            >
+            </v-text-field>
+
+            <v-btn
+                color="success"
+                class="mr-4"
+                @click="create"
+            >
+                Valider
+            </v-btn>
+
+        </v-form>
 
     </v-container>
 
@@ -17,11 +34,25 @@
     export default {
         name: "IdeaCreate",
         data() {
-            return {};
+            return { label: '' };
         },
         created() {
         },
         methods: {
+            create() {
+                const label = this.label;
+                fetch('/api/ideas', {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/ld+json'},
+                    body: JSON.stringify({label})
+                })
+                .then( response => {
+                    console.log(response);
+                })
+                .catch( (err) => {
+                    console.log(err);
+                });
+            }
         }
     }
 
