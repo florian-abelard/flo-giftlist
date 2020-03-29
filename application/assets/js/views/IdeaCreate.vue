4059a8fdaa2672c4d90bb2b23e29215279417869
Florian Abélard <f.abelard@gmail.com>
2020-03-29 23:43:59 +0200

    feat(idea): select recipients when creating an idea

--- application/assets/js/views/IdeaCreate.vue
+++ application/assets/js/views/IdeaCreate.vue
@@ -15,6 +15,19 @@
             >
             </v-text-field>
 
+            <v-autocomplete
+                v-model="recipientsId"
+                :items="allRecipients"
+                item-text="name"
+                item-value="id"
+                outlined
+                dense
+                chips
+                small-chips
+                label="Outlined"
+                multiple
+            ></v-autocomplete>
+
             <v-btn
                 color="success"
                 class="mr-4"
@@ -34,17 +47,24 @@
     export default {
         name: "IdeaCreate",
         data() {
-            return { label: '' };
+            return { label: '', recipientsId: [], allRecipients: []};
         },
         created() {
+            this.fetchRecpients();
         },
         methods: {
-            create() {
+            create()
+            {
                 const label = this.label;
+                const recipientsId = this.recipientsId;
+                const recipientsUri = this.buildUris(recipientsId);
                 fetch('/api/ideas', {
                     method: 'POST',
                     headers: {'Content-Type': 'application/ld+json'},
-                    body: JSON.stringify({label})
+                    body: JSON.stringify({
+                        label: label,
+                        recipients: recipientsUri
+                    })
                 })
                 .then( response => {
                     console.log(response);
@@ -52,6 +72,29 @@
                 .catch( (err) => {
                     console.log(err);
                 });
+            },
+            fetchRecpients()
+            {
+                fetch('/api/recipients')
+                .then( response => {
+                    return response.json();
+                })
+                .then( (data) => {
+                    this.allRecipients = data['hydra:member'];
+                })
+                .catch( (err) => {
+                    console.log(err);
+                });
+            },
+            buildUris(elementsId)
+            {
+                let uris = [];
+
+                elementsId.forEach(id => {
+                    uris.push('/api/recipients/' + id);
+                });
+
+                return uris;
             }
         }
     }
