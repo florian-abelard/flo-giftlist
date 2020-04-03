16e4417b8b2ba7ec63dd2e83809a01ec2299188f
Florian Abélard <f.abelard@gmail.com>
2020-04-03 20:31:45 +0200

    feat(idea): initialize the idea detail page

---
+++ application/assets/js/views/IdeaDetail.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <v-container justify-center>
+
+        <v-container class="display-1">Idées cadeaux</v-container>
+
+        <p>Détail de l'idée cadeau</p>
+        <p>{{ idea.label }}</p>
+
+    </v-container>
+
+</template>
+
+<script>
+    
+    export default {
+        name: "IdeaDetail",
+        data() {
+            return { idea: {} };
+        },
+        created() {
+            this.fetchIdea(this.$route.params.id);
+        },
+        methods: {
+            fetchIdea(id) {
+                fetch('/api/ideas/' + id)
+                .then( response => {
+                    return response.json();
+                })
+                .then( (data) => {
+                    this.idea = data;
+                })
+                .catch( (err) => {
+                    console.log(err);
+                });
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+</style>
