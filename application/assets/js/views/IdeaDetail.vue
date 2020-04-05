97e6a6289c41f539e6989b4772bc5b02e2e6ca65
Florian Abélard <f.abelard@gmail.com>
2020-04-05 20:56:15 +0200

    feat(idea): update an idea [WIP]
    WIP: update only the label of the idea

--- application/assets/js/views/IdeaDetail.vue
+++ application/assets/js/views/IdeaDetail.vue
@@ -4,8 +4,46 @@
 
         <v-container class="display-1">Idées cadeaux</v-container>
 
+        <v-btn
+            color="warning"
+            class="mr-4"
+            @click="$router.go(-1)"
+        >
+            Retour
+        </v-btn>
+        <v-btn
+            color="warning"
+            class="mr-4"
+            @click="editMode = true"
+        >
+            Modifier
+        </v-btn>
+        <br/>
+        <br/>
+
         <p>Détail de l'idée cadeau</p>
-        <p>{{ idea.label }}</p>
+
+        <v-form v-on:submit.prevent="update">
+
+            <v-text-field
+                v-model="idea.label"
+                label="Libellé"
+                :rules="[value => !!value || 'Le libellé est obligatoire']"
+                required
+                :disabled="editMode === false"
+            >
+            </v-text-field>
+
+            <v-btn
+                type="submit"
+                color="success"
+                class="mr-4"
+                :disabled="editMode === false"
+            >
+                Modifier mon idée cadeau
+            </v-btn>
+
+        </v-form>
 
     </v-container>
 
@@ -16,23 +54,43 @@
     export default {
         name: "IdeaDetail",
         data() {
-            return { idea: {} };
+            return { idea: {}, initialIdea: {}, editMode: false};
         },
         created() {
-            this.fetchIdea(this.$route.params.id);
+           this.fetchIdea(this.$route.params.id);
         },
         methods: {
             fetchIdea(id) {
                 fetch('/api/ideas/' + id)
-                .then( response => {
-                    return response.json();
-                })
-                .then( (data) => {
-                    this.idea = data;
-                })
-                .catch( (err) => {
-                    console.log(err);
-                });
+                    .then( response => {
+                        return response.json();
+                    })
+                    .then( (data) => {
+                        this.idea = data;
+                        this.initialIdea = data;
+                    })
+                    .catch( (err) => {
+                        console.log(err);
+                    })
+                ;
+            },
+            update()
+            {
+                const idea = this.idea;
+                fetch('/api/ideas/' + idea.id, {
+                        method: 'PATCH',
+                        headers: {'Content-Type': 'application/merge-patch+json'},
+                        body: JSON.stringify({
+                            label: idea.label
+                        })
+                    })
+                    .then( response => {
+                        console.log(response);
+                    })
+                    .catch( (err) => {
+                        console.log(err);
+                    })
+                ;
             }
         }
     }
