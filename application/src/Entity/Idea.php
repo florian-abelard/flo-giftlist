14e3212f3ed0d2a000c594b9bbca1c136cfe2ecc
Florian Abélard <f.abelard@gmail.com>
2020-09-11 15:19:38 +0200

    style: change white-spaces in annotations

--- application/src/Entity/Idea.php
+++ application/src/Entity/Idea.php
@@ -15,7 +15,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\IdeaRepository")
- * 
+ *
  * @ApiResource(
  *      collectionOperations={"get", "post"},
  *      itemOperations={"get", "put", "delete"},
@@ -36,29 +36,29 @@ class Idea implements TimestampableInterface
      *
      * @ORM\Id
      * @ORM\Column(type="uuid", unique=true)
-     * @ORM\GeneratedValue(strategy="CUSTOM") 
+     * @ORM\GeneratedValue(strategy="CUSTOM")
      * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
-     * 
+     *
      * @Groups({"idea"})
      */
     protected $id;
 
-    /** 
+    /**
      * @var string The label of the gift
-     * 
+     *
      * @ORM\Column(type="string", length=255)
-     * 
      * @Assert\NotBlank
+     *
      * @Groups({"idea"})
      */
     private $label;
 
-    /** 
+    /**
      * @var Price The price of the gift
-     * 
+     *
      * @ORM\Embedded(class="App\Entity\ValueObject\Price")
-     * 
      * @Assert\Type(type="App\Entity\ValueObject\Price")
+     *
      * @Groups({"idea"})
      */
     private $price;
@@ -69,7 +69,7 @@ class Idea implements TimestampableInterface
      *      joinColumns={@ORM\JoinColumn(name="idea_id", referencedColumnName="id")},
      *      inverseJoinColumns={@ORM\JoinColumn(name="recipient_id", referencedColumnName="id")}
      *      )
-     * 
+     *
      * @Groups({"idea"})
      */
     private $recipients;
