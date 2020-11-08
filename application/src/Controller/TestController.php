44ebdcf010e5c3d46e1aa451b52408b44377662f
Florian Abélard <f.abelard@gmail.com>
2020-11-08 17:41:05 +0100

    chore: test apache + php-fpm

--- application/src/Controller/TestController.php
+++ application/src/Controller/TestController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Idea;
-use App\Entity\ValueObject\Price;
-use Doctrine\Common\Util\Debug;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,29 +9,11 @@ class TestController extends AbstractController
 {
     /**
      * @Route("/back/test")
+     *
      * @return Response
      */
     public function test()
     {
         echo '<pre>', print_r('TEST CONTROLLER', 1), '</pre>';
-
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $repository = $this->getDoctrine()->getRepository(Idea::class);
-
-        $idea = $repository->find('211f18e1-6aca-4de7-9284-284bc449cefd');
-
-        echo '<pre>',print_r($idea->getPrice()->value(), 1),'</pre>';
-
-        $idea->setPrice(new Price($idea->getPrice()->value() + 1));
-
-        $entityManager->persist($idea);
-
-        $entityManager->flush();
-
-        echo '<pre>', Debug::dump($idea), '</pre>';
-
-        die('<pre>'. print_r($idea->getPrice()->value(), 1));
-
     }
 }
