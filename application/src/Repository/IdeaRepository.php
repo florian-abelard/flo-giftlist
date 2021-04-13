6ba7c9ed32ff1b4f92377dc00442dba7588573a4
Florian Abélard <f.abelard@gmail.com>
2021-04-13 22:48:59 +0200

    fix (chore): composer update

--- application/src/Repository/IdeaRepository.php
+++ application/src/Repository/IdeaRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Idea;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Idea|null find($id, $lockMode = null, $lockVersion = null)
